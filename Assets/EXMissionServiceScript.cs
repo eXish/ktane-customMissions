5ec3e4761451e6c09556b3cdd46dc6f6ba0599ac
Brendan King <brendan@brendanking.com>
2024-11-28 22:11:02 -0500

    Added 2 new challenge bombs

--- Assets/EXMissionServiceScript.cs
+++ Assets/EXMissionServiceScript.cs
@@ -9,6 +9,8 @@ public class EXMissionServiceScript : MonoBehaviour {
     KeyCode[] eToExplodeKeys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad5, KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8, KeyCode.Keypad9, KeyCode.Keypad0, KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow };
 	string currentMission;
 
+    bool dontDoAgain;
+
     void Awake()
     {
         game.OnStateChange += state => {
@@ -17,7 +19,7 @@ public class EXMissionServiceScript : MonoBehaviour {
     }
 
 	void Update () {
-		if (currentMission == "mod_eXishMissions_etoexplode")
+        if (currentMission == "mod_eXishMissions_etoexplode")
         {
             foreach (KeyCode k in eToExplodeKeys)
             {
@@ -25,7 +27,17 @@ public class EXMissionServiceScript : MonoBehaviour {
                     Detonate();
             }
         }
-	}
+        else if (currentMission == "mod_eXishMissions_elevatorbomb")
+        {
+            if (dontDoAgain)
+                return;
+            if (GameObject.Find("ElevatorBomb(Clone)") == null)
+                Application.Quit();
+            dontDoAgain = true;
+        }
+        else
+            dontDoAgain = false;
+    }
 
     void Detonate()
     {
